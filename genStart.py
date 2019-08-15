f5773004353bb5312e345b412e5b43ecb52e0951
admin <admin@mail.com>
2019-08-15 15:51:06 +0300

    Camera names

--- genStart.py
+++ genStart.py
@@ -3,11 +3,11 @@ from getVideoDeviceNumberById import getVideoDeviceNumberById
 
 # ==================================================
 # argus
-cam1_id = 00000
+cam1_id = "1ddaa10e"
 # uran-9
-cam2_id = 00000
+cam2_id = "7b33851"
 # uav
-cam3_id = 00000
+cam3_id = "798c03c"
 # ==================================================
 
 print('generating start file ...')
@@ -16,12 +16,12 @@ sleep(1)
 f = open('start.bat', 'w')
 f.write('start cmd /c red5\n')
 f.write('powershell sleep 10\n')
-f.write('start cmd /c ffmpeg -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
-        str(getVideoDeviceNumberById(cam1_id)) + ' -i video="USB2.0 PC CAMERA" -f flv rtmp://localhost/oflaDemo/cam1\n')
+f.write('start cmd /c ffmpeg -analyzeduration 0 -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
+        str(getVideoDeviceNumberById(cam1_id)) + ' -i video="USB2.0 PC CAMERA" -tune zerolatency -f flv rtmp://localhost/oflaDemo/cam1\n')
 f.write('powershell sleep 10\n')
-f.write('start cmd /c ffmpeg -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
-        str(getVideoDeviceNumberById(cam2_id)) + ' -i video="USB2.0 PC CAMERA" -f flv rtmp://localhost/oflaDemo/cam2\n')
+f.write('start cmd /c ffmpeg -analyzeduration 0 -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
+        str(getVideoDeviceNumberById(cam2_id)) + ' -i video="USB2.0 PC CAMERA" -tune zerolatency -f flv rtmp://localhost/oflaDemo/cam2\n')
 f.write('powershell sleep 10\n')
-f.write('start cmd /c ffmpeg -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
-        str(getVideoDeviceNumberById(cam3_id)) + ' -i video="USB2.0 PC CAMERA" -f flv rtmp://localhost/oflaDemo/cam3\n')
+f.write('start cmd /c ffmpeg -analyzeduration 0 -f dshow -video_size 640x480 -framerate 30 -video_device_number ' +
+        str(getVideoDeviceNumberById(cam3_id)) + ' -i video="USB2.0 PC CAMERA" -tune zerolatency -f flv rtmp://localhost/oflaDemo/cam3\n')
 f.close()
